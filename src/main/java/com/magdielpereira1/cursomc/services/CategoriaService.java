609de50ec80ef50923c79eb19765d13f745f15b1
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-01-08 13:24:58 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/magdielpereira1/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.magdielpereira1.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magdielpereira1.cursomc.domain.Categoria;
+import com.magdielpereira1.cursomc.repositories.CategoriaRepository;
+
+
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+		
+}
