58b3485490e0ffb80abed5b3bdf2cc85cc6f88eb
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-01-27 11:57:44 -0300

    Endpoint/pedidos/{id} disponibilizado

---
+++ src/main/java/com/magdielpereira1/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.magdielpereira1.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magdielpereira1.cursomc.domain.Pedido;
+import com.magdielpereira1.cursomc.repositories.PedidoRepository;
+
+
+
+@Service
+public class PedidoService {
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new com.magdielpereira1.cursomc.services.exception.ObjectNotFoundException(
+				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+		
+}
