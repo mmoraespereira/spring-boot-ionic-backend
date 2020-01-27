246d7e3f47644713e1d9114301b9b2129f03e48e
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-01-27 14:36:20 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/magdielpereira1/cursomc/services/ClienteService.java
+++ src/main/java/com/magdielpereira1/cursomc/services/ClienteService.java
@@ -15,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new com.magdielpereira1.cursomc.services.exception.ObjectNotFoundException(
 				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Cliente.class.getName()));
