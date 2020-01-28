891995c39213c30faff540da96c6440a740ed151
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-01-28 12:20:20 -0300

    Paginação com parametros adionais na requisição

--- src/main/java/com/magdielpereira1/cursomc/services/CategoriaService.java
+++ src/main/java/com/magdielpereira1/cursomc/services/CategoriaService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.magdielpereira1.cursomc.domain.Categoria;
 import com.magdielpereira1.cursomc.repositories.CategoriaRepository;
 import com.magdielpereira1.cursomc.services.exception.DataIntegrityException;
-
-
 
 @Service
 public class CategoriaService {
@@ -48,4 +49,9 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	} 
 }
