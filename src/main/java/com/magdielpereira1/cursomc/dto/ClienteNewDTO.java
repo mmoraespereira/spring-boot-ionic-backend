70b6c411e64338fd5172c49e1f162e225d348b18
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-02-15 13:09:18 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/magdielpereira1/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/magdielpereira1/cursomc/dto/ClienteNewDTO.java
@@ -2,30 +2,56 @@ package com.magdielpereira1.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.magdielpereira1.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-    private String nome;
-    private String email;
-    private String cpfOuCnpj;
-    private Integer tipo;
-    
-    private String logradouro;
-    private String numero;
-    private String complemento;
-    private String bairro;
-    private String cep;
-    
-    private String telefone1;
-    private String telefone2;
-    private String telefone3;
-    
-    private Integer cidadeId;
-    
-    public ClienteNewDTO() {
-    	
-    }
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String cpfOuCnpj;
+
+	private Integer tipo;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String logradouro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String numero;
+
+	private String complemento;
+
+	private String bairro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String cep;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String telefone1;
+
+	private String telefone2;
+
+	private String telefone3;
+
+	private Integer cidadeId;
+
+	public ClienteNewDTO() {
+
+	}
 
 	public String getNome() {
 		return nome;
@@ -130,5 +156,5 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-       
+
 }
