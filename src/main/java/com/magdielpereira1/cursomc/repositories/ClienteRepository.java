0b6ade65cbbd910316277bd7bf494ed659343932
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-02-17 20:14:29 -0300

    validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/magdielpereira1/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/magdielpereira1/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,15 @@ package com.magdielpereira1.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.magdielpereira1.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
+	
 }
--- src/main/java/com/magdielpereira1/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/magdielpereira1/cursomc/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.magdielpereira1.cursomc.domain.Cliente;
 import com.magdielpereira1.cursomc.domain.enums.TipoCliente;
 import com.magdielpereira1.cursomc.dto.ClienteNewDTO;
+import com.magdielpereira1.cursomc.repositories.ClienteRepository;
 import com.magdielpereira1.cursomc.resources.exception.FieldMessage;
 import com.magdielpereira1.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 
@@ -32,7 +40,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 
 		}
-
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null) {
+        	list.add(new FieldMessage("email", "Email já existente!"));
+        }
+		
 		for (FieldMessage e : list) {
 
 			context.disableDefaultConstraintViolation();
