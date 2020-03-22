f24c3ca7bfe2f5b243d5d817da4a75410d47ab6d
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-03-22 15:15:40 -0300

    Adicionando senha a Cliente

--- src/main/java/com/magdielpereira1/cursomc/domain/Cliente.java
+++ src/main/java/com/magdielpereira1/cursomc/domain/Cliente.java
@@ -21,134 +21,143 @@ import com.magdielpereira1.cursomc.domain.enums.TipoCliente;
 
 @Entity
 public class Cliente implements Serializable {
-	
-   private static final long serialVersionUID = 1L;
-   
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Integer id;
-   private String nome;
-   
-   @Column(unique = true)
-   private String email;
-   
-   private String cpfOuCnpj;
-   private Integer tipo;
-   
-   @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
-   private List<Endereco> enderecos = new ArrayList<>();
-   
-   @ElementCollection
-   @CollectionTable(name="TELEFONE")
-   private Set<String> telefones = new HashSet<String>();
-   
-   @JsonIgnore
-   @OneToMany(mappedBy = "cliente")
-   private List<Pedido> pedidos = new ArrayList<>();
-   
-   public Cliente() {
-	   
-   }
 
-public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
-	super();
-	this.id = id;
-	this.nome = nome;
-	this.email = email;
-	this.cpfOuCnpj = cpfOuCnpj;
-	this.tipo = (tipo == null) ? null : tipo.getCod();
-}
+	private static final long serialVersionUID = 1L;
 
-public Integer getId() {
-	return id;
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	@Column(unique = true)
+	private String email;
 
-public String getNome() {
-	return nome;
-}
+	private String cpfOuCnpj;
+	private Integer tipo;
 
-public void setNome(String nome) {
-	this.nome = nome;
-}
+	@JsonIgnore
+	private String senha;
 
-public String getEmail() {
-	return email;
-}
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Endereco> enderecos = new ArrayList<>();
 
-public void setEmail(String email) {
-	this.email = email;
-}
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<String>();
 
-public String getCpfOuCnpj() {
-	return cpfOuCnpj;
-}
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
-public void setCpfOuCnpj(String cpfOuCnpj) {
-	this.cpfOuCnpj = cpfOuCnpj;
-}
+	public Cliente() {
 
-public TipoCliente getTipo() {
-	return TipoCliente.toEnum(tipo);
-} 	
+	}
 
-public void setTipo(TipoCliente tipo) {
-	this.tipo = tipo.getCod();
-}
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = (tipo == null) ? null : tipo.getCod();
+		this.senha = senha;
+	}
 
-public List<Endereco> getEnderecos() {
-	return enderecos;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public void setEnderecos(List<Endereco> enderecos) {
-	this.enderecos = enderecos;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public Set<String> getTelefones() {
-	return telefones;
-}
+	public String getNome() {
+		return nome;
+	}
 
-public void setTelefones(Set<String> telefones) {
-	this.telefones = telefones;
-}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-public List<Pedido> getPedidos() {
-	return pedidos;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public void setPedidos(List<Pedido> pedidos) {
-	this.pedidos = pedidos;
-}
-  
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
 
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	return result;
-}
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Cliente other = (Cliente) obj;
-	if (id == null) {
-		if (other.id != null)
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
 			return false;
-	} else if (!id.equals(other.id))
-		return false;
-	return true;
-}
-
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
--- src/main/java/com/magdielpereira1/cursomc/services/DBService.java
+++ src/main/java/com/magdielpereira1/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.magdielpereira1.cursomc.domain.Categoria;
@@ -51,6 +52,8 @@ public class DBService {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private BCryptPasswordEncoder pe; 
 
 	public void instantiateDatabase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -109,7 +112,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "mpereira950@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "mpereira950@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
