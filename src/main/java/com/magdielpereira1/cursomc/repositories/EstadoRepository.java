0394213120019b27d30618ff6fbe7353b33b16e6
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-03-23 23:32:30 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/magdielpereira1/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/magdielpereira1/cursomc/repositories/EstadoRepository.java
@@ -1,11 +1,14 @@
 package com.magdielpereira1.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.magdielpereira1.cursomc.domain.Estado;
 
+
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
-
+	public List<Estado> findAllByOrderByNome();
 }
---
+++ src/main/java/com/magdielpereira1/cursomc/services/CidadeService.java
@@ -0,0 +1,19 @@
+package com.magdielpereira1.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magdielpereira1.cursomc.domain.Cidade;
+import com.magdielpereira1.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+	@Autowired
+	private CidadeRepository repo;
+
+	public List<Cidade> findByEstado(Integer estadoId) {
+		return repo.findCidades(estadoId);
+	}
+}
