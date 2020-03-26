ad9dec6fe85ab7bc22035d1fbaf6703a15f6c9ac
Magdiel Pereira <magdielpereira1@hotmail.com>
2020-03-25 21:31:52 -0300

    Padronizando formato das exceções

--- src/main/java/com/magdielpereira1/cursomc/resources/exception/ResourceExceptionHandller.java
+++ src/main/java/com/magdielpereira1/cursomc/resources/exception/ResourceExceptionHandller.java
@@ -16,32 +16,35 @@ import com.magdielpereira1.cursomc.services.exception.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandller {
 	
-     @ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-	} 
-     
-    @ExceptionHandler(DataIntegrityException.class)
- 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
- 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
- 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
- 	} 
-    
-    
-    
-    @ExceptionHandler(MethodArgumentNotValidException.class)
- 	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
- 		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de validação", System.currentTimeMillis());
- 		for(FieldError x: e.getBindingResult().getFieldErrors()) {
- 			err.addError(x.getField(), x.getDefaultMessage());
- 		}
- 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
- 	} 
-    
-    @ExceptionHandler(AuthorizationException.class)
-   	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request){
-   		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
-   		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
-   	} 
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", e.getMessage(), request.getRequestURI());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
+
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Acesso negado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+
 }
